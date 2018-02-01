eb3c45c63ebc97c2df89c5a9717534c1462ea4c3
joshua1234515 <joshua_jimenez@davincischools.org>
2018-02-01 14:41:07 -0800

    Add files via upload

---
+++ music-cheerleader.rb
@@ -0,0 +1,34 @@
+use_bpm 100
+synth_names:growl
+with_fx:compressor do
+  play:g4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:d4
+  sleep 1
+  play:g4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:f4
+  sleep 1
+  play:d4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:b4
+  sleep 0.25
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:as
+  sleep 0.5
+  play:as
+  sleep 0.5
+  play:g4
+  sleep 0.5
+end
